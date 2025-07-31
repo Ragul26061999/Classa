b2b7de5159d6e2fa16bbacd7d8fbe82c7c740b14
Ragul <ragul26061999@gmail.com>
2025-07-31 19:24:15 +0530

    Add new files

--- app/whychooseclassa/page.tsx
+++ app/whychooseclassa/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { useState, useEffect } from "react";
 import { motion, useScroll, useTransform, useMotionTemplate, useMotionValue, useSpring } from "framer-motion";
